5ed9d17fb6ef2d939c940bad5e6c3f131fc9a39d
Leonardo Valle <leonardovalle18@gmail.com>
2026-06-07 17:39:06 -0300

    removing erdirect

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.nexustecnologia.online" }],
-        destination: "https://nexustecnologia.online/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
